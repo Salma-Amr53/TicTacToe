2d32d786115486d4d49a2d5fce1c801185a0d988
lasheen20 <lasheen29@gmail.com>
2025-09-25 02:16:46 +0300

    finalizing

--- lasheen.cpp
+++ lasheen.cpp
@@ -64,13 +64,112 @@ void setName(const string& name)
 /////////////////////////////////
 
 //getBestMove
+void getBestMove(Board& board,int& row,int& col)
+{
+	char ai=this->getSymbol();
+	char human;
+	if(ai=='X')
+		human='O';
+	else
+		human='X'; 
+	int bestScore=-5000;
+	int size=board.getSize();
+
+	for(int i=0;i<size;i++)
+	{
+		for(int j=0;j<size;j++)
+		{
+			 if (board.isValidMove(i, j)) {
+                board.makeMove(i, j, ai);
+                int score = minimax(board, 0, false, ai, human);
+                board.makeMove(i, j, ' ');    //for integration it might not work so we need a function to empty the space after trying a move
+
+                if (score > bestScore) {
+                    bestScore = score;
+                    row = i;
+                    col = j;
+				}
+			}
+		}	
+	}
+}
+
+int minimax(Board& board, int depth, bool isMaximizing, char ai, char human) 
+{
+    int score = evaluateBoard(board, ai, human);
+
+    if (score == 10) return score - depth;
+    if (score == -10) return score + depth;
+    if (board.isFull()) return 0;
+
+    int size = board.getSize();
+
+    if (isMaximizing) {
+        int best = -1000;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board.isValidMove(i, j)) {
+                    board.makeMove(i, j, ai);
+                    best = max(best, minimax(board, depth + 1, false, ai, human));
+                    board.undoMove(i, j);
+                }
+            }
+        }
+        return best;
+    } else {
+        int best = 1000;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board.isValidMove(i, j)) {
+                    board.makeMove(i, j, human);
+                    best = min(best, minimax(board, depth + 1, true, ai, human));
+                    board.undoMove(i, j);
+                }
+            }
+        }
+        return best;
+    }
+}
 
 /////////////////////////////////
 
 //Start
+void start() {
+    board.reset();            
+    showMenu();                 
+
+    currentPlayer = player1;    
+
+    while (!checkGameEnd()) {   
+        board.display();
+        std::cout << currentPlayer->getName() 
+                  << " (" << currentPlayer->getSymbol() << ")'s turn.\n";
+
+        int row, col;
+
+        if (AIPlayer* ai = dynamic_cast<AIPlayer*>(currentPlayer)) {
+            handleAIMove(ai);   
+        } else {
+            handleHumanMove(currentPlayer); 
+        }
+
+        switchPlayer();      
+    }
+
+    board.display();          
+    displayResult();         
+}
+
 
 /////////////////////////////////
 
 //switchPlayer
+void switchPlayer() {
+    if (currentPlayer == player1)
+        currentPlayer = player2;
+    else
+        currentPlayer = player1;
+}
+
 
 /////////////////////////////////
